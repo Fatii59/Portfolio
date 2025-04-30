5158c66d3fdc68a89f3bfa754fbfe048ebb90dc3
Fatii59 <93334642+Fatii59@users.noreply.github.com>
2025-04-30 21:06:41 +0200

    fix: use HashRouter instead of BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Footer from "./components/Footer";
-import Navbar from"./components/Navbar";
+import Navbar from "./components/Navbar";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
-    <main className='bg-slate-300/20'>
+    <main className="bg-slate-300/20">
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/Portfolio/' element={<Home />} />
-          <Route
-            path='/*'
-            element={
-              <>
-                <Routes>
-                  <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/contact' element={<Contact />} />
-                </Routes>
-                <Footer />
-              </>
-            }
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
